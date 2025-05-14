402bdf8784229bf9e9b2dabc3427766260cf8cdc
tjamad <99321922+tjamad@users.noreply.github.com>
2025-05-14 17:58:50 +0200

    Update Vite configuration

--- Individualisierung/project/src/components/InvoiceStyles/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as SmartInvoice } from './SmartInvoice';
-export { default as ClassicInvoice } from './ClassicInvoice';
-export { default as CreativeInvoice } from './CreativeInvoice';
-export { default as CraftsmanInvoice } from './CraftsmanInvoice';
-export { default as ModernInvoice } from './ModernInvoice';
